4af90539815c487256463ba7c2549850c66dcbb2
Aasrita Sangani <aasritasanganipro07@gmail.com>
2025-09-18 10:55:36 +0530

    Add files via upload

---
+++ datatypes.cpp
@@ -0,0 +1,16 @@
+//C++ data types ==> Primitive, Derived, User defined
+//Primitive ==> int, float, double etc.
+//Derived ==> array, pointer, reference, functions etc.
+//User defined ==> struct, class, enum etc.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	string lastname = "Sangani";
+	string firstname;
+	cout << "Enter your firstname: " << firstname;
+	cin >> firstname;
+	cout << lastname;
+}
+

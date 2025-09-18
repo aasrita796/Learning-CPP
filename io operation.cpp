ed9a2357f05368469ee9dc611de8098b3f5a49a9
Aasrita Sangani <aasritasanganipro07@gmail.com>
2025-09-18 10:31:00 +0530

    Add files via upload

---
+++ io operation.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std; //instead of using std individually in each step, we use this
+#include <bits/stdc++.h> 
+
+int main(){
+	cout <<"Hey! Aasrita." <<"\n"; 
+	int x,y;
+	cin >> x >> y;
+	cout << "Value of x is: " <<x << "and Value of y is: " <<y;
+	return 0;
+}
